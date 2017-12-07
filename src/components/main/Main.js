f0c2dbfaeb609a624904cd0f25027b4fa4484667
budaman <budamantvydas@gmail.com>
2017-12-07 18:08:04 +0200

    added side bar

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Drawer from "material-ui/Drawer";
+import AppBar from "material-ui/AppBar";
+import RaisedButton from "material-ui/RaisedButton";
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: "#bda26d"
+  }
+});
 
 class Main extends Component {
   render() {
-    return <div>Main is working</div>;
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <Drawer width={200}>
+            <AppBar title="AppBar" />
+          </Drawer>
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
