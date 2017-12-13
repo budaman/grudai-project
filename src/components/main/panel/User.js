e4370bfbd6dff9014ed830e255df820d4729789c
budaman <budamantvydas@gmail.com>
2017-12-13 10:20:42 +0200

    charts has been made

--- src/components/main/panel/User.js
+++ src/components/main/panel/User.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Steper from "./Steper";
 import MoneyBar from "./MoneyBar";
 import { List, ListItem } from "material-ui/List";
+import Divider from "material-ui/Divider";
 
 class User extends Component {
   state = {
@@ -30,9 +31,10 @@ class User extends Component {
             <MoneyBar />
           </div>
         </div>
+
         <div className="statistic-container">
           <div className="stats-window-big">
-            <h5>Augalų rūšys</h5>
+            <h5>Lorem ipsum</h5>
             <div className="stats-cont">
               <div className="diagram-cont">
                 <img
@@ -53,12 +55,33 @@ class User extends Component {
             </div>
           </div>
           <div className="stats-window-big">
-            <h5>Grūdų kokybė</h5>
+            <h5>Lorem ipsum</h5>
             <div className="stats-cont">
               <div className="diagram-cont">
                 <img
                   className="statistic-img"
                   src="assets/static/2.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                  <li>dolor</li>
+                  <li>sit</li>
+                  <li>amet</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/3.png"
                   alt="static pic"
                 />
               </div>
@@ -76,7 +99,7 @@ class User extends Component {
               <div className="diagram-cont">
                 <img
                   className="statistic-img"
-                  src="assets/static/2.png"
+                  src="assets/static/4.png"
                   alt="static pic"
                 />
               </div>
@@ -94,7 +117,7 @@ class User extends Component {
               <div className="diagram-cont">
                 <img
                   className="statistic-img"
-                  src="assets/static/2.png"
+                  src="assets/static/5.png"
                   alt="static pic"
                 />
               </div>
@@ -106,12 +129,296 @@ class User extends Component {
               </div>
             </div>
           </div>
-          <div className="stats-window" />
-          <div className="stats-window" />
-          <div className="stats-window" />
-          <div className="stats-window" />
-          <div className="stats-window" />
-          <div className="stats-window" />
+
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/6.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/7.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/8.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/10.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/11.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/12.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/13.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/14.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/15.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/16.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/17.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/18.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/19.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/20.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/21.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div className="stats-window-big">
+            <h5>Grūdų kokybė</h5>
+            <div className="stats-cont">
+              <div className="diagram-cont">
+                <img
+                  className="statistic-img"
+                  src="assets/static/22.png"
+                  alt="static pic"
+                />
+              </div>
+              <div className="list-container">
+                <ol>
+                  <li>Lorem</li>
+                  <li>ipsum</li>
+                </ol>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
