a67291d7eb4c1735d9f3cde3f0e7d570f89d8666
budaman <budamantvydas@gmail.com>
2017-12-14 10:16:39 +0200

    new layout getting look good

--- src/components/main/panel/User.js
+++ src/components/main/panel/User.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import ActionCreditCard from "material-ui/svg-icons/action/credit-card";
+import ActionChromeReaderMode from "material-ui/svg-icons/action/chrome-reader-mode";
 
 class User extends Component {
   state = {
@@ -12,7 +14,46 @@ class User extends Component {
   };
 
   render() {
-    return <div className="user-panel">aaaaaaaa</div>;
+    return (
+      <div className="user-panel">
+        <div className="static-cont">
+          <div className="main-settings static-bar">
+            <div className="statistic-icon">
+              <div className="statistic-icon-round">
+                <ActionChromeReaderMode
+                  style={{
+                    width: 22,
+                    height: 19
+                  }}
+                  color="white"
+                />
+              </div>
+            </div>
+            <div className="statistic-label">Kredito ir Debito istorija</div>
+          </div>
+          <div className="main-settings schedule-bar static-bar-header">
+            <div className="statistic-icon">
+              <div className="statistic-icon-round">
+                <ActionChromeReaderMode
+                  style={{
+                    width: 22,
+                    height: 19
+                  }}
+                  color="white"
+                />
+              </div>
+            </div>
+            <div className="statistic-label">Peržiūrėti operacijų istoriją</div>
+          </div>
+        </div>
+        <div className="static-cont main-statistic">
+          <div className="main-settings static-bar statistic-main-icon ">
+            <img src="assets/static/statistic.png" alt="statistic" />
+          </div>
+          <div className="main-settings schedule-bar">1</div>
+        </div>
+      </div>
+    );
   }
 }
 
