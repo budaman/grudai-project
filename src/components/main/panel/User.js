c32394ac020670bf68a5f02f802f7992fdcf704f
budaman <budamantvydas@gmail.com>
2017-12-14 16:27:06 +0200

    updated content box

--- src/components/main/panel/User.js
+++ src/components/main/panel/User.js
@@ -4,6 +4,8 @@ import ActionChromeReaderMode from "material-ui/svg-icons/action/chrome-reader-m
 import DeviceDataUsage from "material-ui/svg-icons/device/data-usage";
 import DeviceGraphicEq from "material-ui/svg-icons/device/graphic-eq";
 import DeviceStorage from "material-ui/svg-icons/device/storage";
+import ActionEuroSymbol from "material-ui/svg-icons/action/euro-symbol";
+import AlertWarning from "material-ui/svg-icons/alert/warning";
 
 class User extends Component {
   state = {
@@ -19,110 +21,6 @@ class User extends Component {
   render() {
     return (
       <div className="user-panel">
-        <div className="static-cont container-margin">
-          <div className="main-settings static-bar">
-            <div className="statistic-icon">
-              <div className="statistic-icon-round">
-                <ActionChromeReaderMode
-                  style={{
-                    width: 22,
-                    height: 19
-                  }}
-                  color="white"
-                />
-              </div>
-            </div>
-            <div className="statistic-label">Kredito ir Debeto istorija</div>
-          </div>
-          <div className="main-settings schedule-bar static-bar-header">
-            <div className="statistic-icon">
-              <div className="statistic-icon-round">
-                <ActionChromeReaderMode
-                  style={{
-                    width: 22,
-                    height: 19
-                  }}
-                  color="white"
-                />
-              </div>
-            </div>
-            <div className="statistic-label">Peržiūrėti operacijų istoriją</div>
-          </div>
-        </div>
-        <div className="static-cont main-statistic">
-          <div className="main-settings static-bar statistic-main-icon ">
-            <img
-              className="main-statistic-img"
-              src="assets/static/statistic.png"
-              alt="statistic"
-            />
-          </div>
-          <div className="main-settings schedule-bar statistic-main-icon">
-            <img
-              className="document-img"
-              src="assets/static/documents.png"
-              alt="statistic"
-            />
-          </div>
-        </div>
-        <div className="diagram-header container-margin static-cont">
-          <div className="diagram-bar main-settings">
-            {" "}
-            <div className="statistic-icon">
-              <div className="statistic-icon-round">
-                <DeviceDataUsage
-                  style={{
-                    width: 22,
-                    height: 19
-                  }}
-                  color="white"
-                />
-              </div>
-            </div>
-            <div className="statistic-label">Sugalvoti pavadinima</div>
-          </div>
-          <div className="diagram-bar main-settings">
-            {" "}
-            <div className="statistic-icon">
-              <div className="statistic-icon-round">
-                <DeviceGraphicEq
-                  style={{
-                    width: 22,
-                    height: 19
-                  }}
-                  color="white"
-                />
-              </div>
-            </div>
-            <div className="statistic-label">Sugalvoti pavadinima</div>
-          </div>
-          <div className="diagram-bar main-settings">
-            {" "}
-            <div className="statistic-icon">
-              <div className="statistic-icon-round">
-                <DeviceStorage
-                  style={{
-                    width: 22,
-                    height: 19
-                  }}
-                  color="white"
-                />
-              </div>
-            </div>
-            <div className="statistic-label">Sugalvoti pavadinima</div>
-          </div>
-        </div>
-        <div className="diagram-header static-cont">
-          <div className="diagram-container main-settings">
-            <img src="assets/static/5.png" />
-          </div>
-          <div className="diagram-container main-settings">
-            <img src="assets/static/3.png" />{" "}
-          </div>
-          <div className="diagram-container main-settings">
-            <img src="assets/static/2.png" />{" "}
-          </div>
-        </div>
         <div className="container-margin boxes-display">
           <div className="container-big-box">
             <div className="main-settings box-header">
@@ -137,13 +35,154 @@ class User extends Component {
                   />
                 </div>
               </div>
-              <div className="statistic-label">Kredito ir Debeto istorija</div>
+              <div className="statistic-label">Kredito ir Debeto Istorija</div>
+            </div>
+            <div className="main-settings box-content-big">
+              <div className="agenda-box">
+                <div className="agenda-cont">
+                  <div style={{ fontSize: 12, letterSpacing: 1 }}>kreditas</div>{" "}
+                  <div style={{ color: "#78c383" }}>30 000.53€</div>
+                </div>
+                <div className="agenda-cont">
+                  <div style={{ fontSize: 12, letterSpacing: 1 }}>debetas</div>{" "}
+                  <div style={{ color: "#eec500" }}>12 521.21€</div>
+                </div>
+                <div className="agenda-cont">
+                  <div className="agenda-cont">
+                    <div style={{ fontSize: 12, letterSpacing: 1 }}>
+                      laikotarpis
+                    </div>{" "}
+                    <div style={{ color: "#698eab" }}>3 metai</div>
+                  </div>
+                </div>
+              </div>
+              <div class="statistic-img-cont">
+                <img
+                  className="main-statistic-img"
+                  src="assets/static/statistic.png"
+                  alt="statistic"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container-small-box">
+            <div className="main-settings box-header">
+              <div className="box-header-icon">
+                <div className="statistic-icon-round">
+                  <ActionChromeReaderMode
+                    style={{
+                      width: 22,
+                      height: 19
+                    }}
+                    color="white"
+                  />
+                </div>
+              </div>
+              <div>Peržiūrėti Operacijų Istoriją</div>
+            </div>
+            <div className="main-settings documents-content-box">
+              <div className="documents-img-container">
+                <img
+                  className="document-img"
+                  src="assets/static/documents.png"
+                  alt="statistic"
+                />
+              </div>
+              <div className="alert-document-box">
+                <AlertWarning />{" "}
+                <span style={{ marginLeft: 5 }}>
+                  yra nebaigtos 3 operacijos{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="boxes-display container-margin">
+          <div className="container-small-box">
+            <div className="main-settings box-header">
+              <div className="box-header-icon">
+                <div className="statistic-icon-round">
+                  <DeviceDataUsage
+                    style={{
+                      width: 22,
+                      height: 19
+                    }}
+                    color="white"
+                  />
+                </div>
+              </div>
+              <div className="statistic-label">Pelno Augimas</div>
+            </div>
+            <div className="main-settings box-content-middle">
+              <div className="diagram-container">
+                <img src="assets/static/5.png" />
+              </div>
+            </div>
+          </div>
+          <div className="container-small-box">
+            <div className="main-settings box-header">
+              <div className="box-header-icon">
+                <div className="statistic-icon-round">
+                  <DeviceGraphicEq
+                    style={{
+                      width: 22,
+                      height: 19
+                    }}
+                    color="white"
+                  />
+                </div>
+              </div>
+              <div className="statistic-label">Paslaugų Kiekis</div>
+            </div>
+            <div className="main-settings box-content-middle">
+              <div className="diagram-container">
+                <img src="assets/static/3.png" />
+              </div>
+            </div>
+          </div>
+          <div className="container-small-box">
+            <div className="main-settings box-header">
+              <div className="box-header-icon">
+                <div className="statistic-icon-round">
+                  <DeviceStorage
+                    style={{
+                      width: 22,
+                      height: 19
+                    }}
+                    color="white"
+                  />
+                </div>
+              </div>
+              <div className="statistic-label">Augalų Rūšys</div>
+            </div>
+            <div className="main-settings box-content-middle">
+              <div className="diagram-container">
+                <img src="assets/static/2.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container-margin boxes-display">
+          <div className="container-big-box">
+            <div className="main-settings box-header">
+              <div className="box-header-icon">
+                <div className="statistic-icon-round">
+                  <ActionEuroSymbol
+                    style={{
+                      width: 22,
+                      height: 19
+                    }}
+                    color="white"
+                  />
+                </div>
+              </div>
+              <div className="statistic-label">Pardavimų Istorija</div>
             </div>
             <div className="main-settings box-content-big">
               {" "}
               <img
                 className="main-statistic-img"
-                src="assets/static/statistic.png"
+                src="assets/static/graph.png"
                 alt="statistic"
               />
             </div>
@@ -161,7 +200,7 @@ class User extends Component {
                   />
                 </div>
               </div>
-              <div>Peržiūrėti operacijų istoriją</div>
+              <div>Peržiūrėti Operacijų Istoriją</div>
             </div>
             <div className="main-settings box-content-big">
               <img
@@ -171,6 +210,10 @@ class User extends Component {
               />
             </div>
           </div>
+        </div>
+
+        <div className="margin-top">
+          <hr />
         </div>
       </div>
     );
